a4dd68eea9ff4fea045eee54ffb596c125ada17f
Juan A <j.alejandrominaya@gmail.com>
2025-06-15 13:15:04 +0200

    feat: Launc subprocess to retrieve aerospace workspace count

--- src/main.c
+++ src/main.c
@@ -5,10 +5,13 @@
 
 #define DEFAULT_LABEL_COLOR "0xFFFFFFFF"
 #define SELECTED_LABEL_COLOR "0xFF00A3CB"
+#define TOTAL_WORKSPACES 10
+#define WORKSPACE_ID_SIZE 32
 
 short workspaces_in_use = 0;
 
-void handler(env env) {
+
+void paint_current_workspace(env env) {
     char* previous_workspace = env_get_value_for_key(env, "PREVIOUS_WORKSPACE");
     char* focused_workspace = env_get_value_for_key(env, "FOCUSED_WORKSPACE");
 
@@ -23,29 +26,67 @@ void handler(env env) {
     snprintf(command, 256, "--set space.%s label.color=" SELECTED_LABEL_COLOR " drawing=on", focused_workspace);
     sketchybar(command);
 
-    snprintf(command, 256, "--set space.%s label.color=" DEFAULT_LABEL_COLOR " drawing=on", previous_workspace);
+    snprintf(command, 256, "--set space.%s label.color=" DEFAULT_LABEL_COLOR, previous_workspace);
     sketchybar(command);
+}
 
-    workspaces_in_use &= ~previous_workspace_update;
-    workspaces_in_use |= focused_workspace_update;
 
-    for(short i = 0; i < 0x1; i++) {
-        if ((workspaces_in_use >> i) & 0b1) {
-            snprintf(command, 256, "--set space.%s drawing=on", previous_workspace);
-        }
-        else {
-            snprintf(command, 256, "--set space.%s drawing=off", previous_workspace);
-        }
-        sketchybar(command);
+void main_handler(env env) {
+    paint_current_workspace(env);
+}
+
+pid_t launch_workspace_watcher(short workspace_id) {
+    pid_t pid = fork();
+
+    // Error with fork
+    if (pid == -1) {
+        perror("Failed to create watcher");
+        exit(-1);
     }
 
+    // Parent process
+    if (pid > 0) {
+        return pid;
+    }
+
+    // Child process
+    printf("Hi, I'm process %i\n", workspace_id);
+    pid_t aerospace_cmd_pid = fork();
+
+    // Error with aerospace command
+    if (aerospace_cmd_pid == -1) {
+        perror("Failed to fork into aerospace workspace command");
+        exit(-1);
+    }
+
+    // Still child process, i.e. pid
+    if (aerospace_cmd_pid > 0) {
+        wait(&aerospace_cmd_pid);
+        exit(0);
+    }
+
+    char workspace[WORKSPACE_ID_SIZE];
+    snprintf(workspace, sizeof(workspace_id), "%i", workspace_id);
+
+    // Grandchild process, i.e. aerospace_cmd_pid
+    execlp("aerospace", "aerospace", "list-windows", "--workspace", workspace, "--count", NULL);
+    perror("Failed to launch aerospace command");
+
+    exit(0);
+}
+
+void start_watching() {
+    pid_t watchers_pids[TOTAL_WORKSPACES];
+    for (short i = 1; i <= TOTAL_WORKSPACES; i++) {
+        watchers_pids[(i-1)] = launch_workspace_watcher(i);
+    }
+
+    for (short i = 1; i <= TOTAL_WORKSPACES; i++) wait(NULL);
 }
 
 int main(int argc, char** argv) {
-    if (argc == 1) {
-        return -1;
-    }
+    // event_server_begin(main_handler, "ash");
 
-    event_server_begin(handler, "ash");
+    start_watching();
     return 0;
 }
